645b21f583f0d4d5ec05b2125798c7168f43dfcc
Mohammadhossein Malekpour <mohammadhossein.malekpour@gmail.com>
2025-07-15 09:42:30 -0400

    solve 0704 in recursive way

--- problems/0704_binary_search/main.py
+++ problems/0704_binary_search/main.py
@@ -1,3 +1,4 @@
+# Iterative binary search - Time: O(log n) Space: O(1)
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         L, R = 0, len(nums) - 1
@@ -12,3 +13,21 @@ class Solution:
                 return M
 
         return -1
+
+
+# Recursive binary search - Time: O(log n) Space: O(log n)
+class Solution:
+    def binary_search(self, l, r, nums, target):
+        if l > r:
+            return -1
+        m = (l + r) // 2
+
+        if target == nums[m]:
+            return m
+        elif target < nums[m]:
+            return self.binary_search(l, m - 1, nums, target)
+        else:
+            return self.binary_search(m + 1, r, nums, target)
+
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binary_search(l=0, r=len(nums)-1, nums=nums, target=target)
